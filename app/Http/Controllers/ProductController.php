331e9a61588d7894f72e62b045c14377197e8389
Apixelian <pixe366@gmail.com>
2026-03-03 01:41:10 -0500

    homework 5

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,50 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index', ['url' => route('products.create') ]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function edit(string $id)
+    {
+        return view('products.edit');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,4 +3,8 @@
 <head>
     <title>Product View</title>
 </head>
+<body>
+    <h1>Products</h1>
+    <a href="{{ $url }}">Add Product</a>
+</body>
 </html>

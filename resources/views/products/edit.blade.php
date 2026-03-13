483cf5d15cbbe5dc204e4d0bfdcc9c8431d9cab2
Apixelian <pixe366@gmail.com>
2026-03-13 13:28:12 -0500

    Changes V2

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Editing</title>
+</head>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product View</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <a href="{{ $url }}">Add Product</a>
+</body>
+</html>
